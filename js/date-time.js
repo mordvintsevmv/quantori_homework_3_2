0ab5323c016daca9981f4d9b30467fe32cdbc8d9
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-24 18:41:01 +0400

    Refactoring: renaming clock to watch; implementing CASIO font

--- js/date-time.js
+++ js/date-time.js
@@ -6,9 +6,9 @@ const seconds = date.getSeconds()
 
 const delay_seconds = hours*60*60 + minutes*60 + seconds
 
-document.getElementById('clock-hand-hours').style.animation = `animate-clock-hand 43200s linear -${delay_seconds}s infinite`;
-document.getElementById('clock-hand-minutes').style.animation = `animate-clock-hand 3600s linear -${delay_seconds}s infinite`;
-document.getElementById('clock-hand-seconds').style.animation = `animate-clock-hand 60s steps(60) -${delay_seconds}s infinite`;
+document.getElementById('watch-hand-hours').style.animation = `animate-watch-hand 43200s linear -${delay_seconds}s infinite`;
+document.getElementById('watch-hand-minutes').style.animation = `animate-watch-hand 3600s linear -${delay_seconds}s infinite`;
+document.getElementById('watch-hand-seconds').style.animation = `animate-watch-hand 60s steps(60) -${delay_seconds}s infinite`;
 
 const checkTime = () => {
     const date = new Date()
@@ -17,8 +17,8 @@ const checkTime = () => {
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
-    document.getElementById('clock-digital-hm').textContent = hours + ":"  + minutes
-    document.getElementById('clock-digital-seconds').textContent = seconds < 10 ? "0"+seconds : seconds
+    document.getElementById('watch-digital-hm').textContent = hours + ":"  + minutes
+    document.getElementById('watch-digital-seconds').textContent = seconds < 10 ? "0"+seconds : seconds
 
     setTimeout(checkTime, 1000)
 
@@ -31,14 +31,14 @@ for (let i=0; i<60; i++){
     let stroke = document.createElementNS('http://www.w3.org/2000/svg', 'use');
 
     if (i%5 === 0){
-        stroke.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#clock-12-stroke')
+        stroke.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#watch-12-stroke')
         stroke.setAttribute("transform", `rotate(${i*6}, 0, 0)`)
     }
     else {
-        stroke.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#clock-60-stroke')
+        stroke.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#watch-60-stroke')
         stroke.setAttribute("transform", `rotate(${i*6}, 0, 0)`)
     }
 
-    document.getElementById('clock').appendChild(stroke)
+    document.getElementById('watch').appendChild(stroke)
 
 }
