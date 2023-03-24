88ed64a696468967bac0dbd26d09a80f903b09d8
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-03-24 05:38:59 +0400

    Feature: Implementing CASIO MTA-1000 design

--- js/date-time.js
+++ js/date-time.js
@@ -3,7 +3,6 @@ const date = new Date()
 const hours = date.getHours()
 const minutes = date.getMinutes()
 const seconds = date.getSeconds()
-const day = date.getDate()
 
 const delay_seconds = hours*60*60 + minutes*60 + seconds
 
@@ -11,4 +10,35 @@ document.getElementById('clock-hand-hours').style.animation = `animate-clock-han
 document.getElementById('clock-hand-minutes').style.animation = `animate-clock-hand 3600s linear -${delay_seconds}s infinite`;
 document.getElementById('clock-hand-seconds').style.animation = `animate-clock-hand 60s steps(60) -${delay_seconds}s infinite`;
 
-document.getElementById('clock-date-text').textContent = day.toString()
+const checkTime = () => {
+    const date = new Date()
+
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+
+    document.getElementById('clock-digital-hm').textContent = hours + ":"  + minutes
+    document.getElementById('clock-digital-seconds').textContent = seconds < 10 ? "0"+seconds : seconds
+
+    setTimeout(checkTime, 1000)
+
+}
+checkTime()
+
+// Creating 60 strokes for every minute
+for (let i=0; i<60; i++){
+
+    let stroke = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+
+    if (i%5 === 0){
+        stroke.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#clock-12-stroke')
+        stroke.setAttribute("transform", `rotate(${i*6}, 0, 0)`)
+    }
+    else {
+        stroke.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#clock-60-stroke')
+        stroke.setAttribute("transform", `rotate(${i*6}, 0, 0)`)
+    }
+
+    document.getElementById('clock').appendChild(stroke)
+
+}
